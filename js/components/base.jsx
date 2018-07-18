5462935db8d1c20acfa69f4dd389bee88f1f87de
orszolka108 <orszolka108@gmail.com>
2018-07-18 10:13:55 +0200

    Mapbox added to main-page with users geolocation

--- js/components/base.jsx
+++ js/components/base.jsx
@@ -4,11 +4,35 @@ import {MainPage} from './main_page.jsx'
 
 
 class Base extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            lng: 0,
+            lat: 0
+        }
+    }
+
+    componentDidMount(){
+        // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/Using_geolocation
+        // Tu pobierasz pozycje
+        navigator.geolocation.getCurrentPosition(position => {
+            this.setState({
+                lng: position.coords.longitude,
+                lat: position.coords.latitude
+            });
+        }, error => {
+            console.log(error)
+        });
+        // Tutaj pobierz dane o dostepnych zakupach
+        // Np z tablicy, pliku czy bazy (firebase)
+    }
     render() {
+
         return <div className="container">
                     <div className="window">
                         <NavBar />
-                        <MainPage />
+                        {/*Tu przekaz pobrana liste zakupow*/}
+                        <MainPage lat={this.state.lat} lng={this.state.lng}/>
                     </div>
                 </div>
     }
--- js/components/main_page.jsx
+++ js/components/main_page.jsx
@@ -2,39 +2,55 @@ import React from 'react';
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
 
+
 const Map = ReactMapboxGl({
     accessToken: "pk.eyJ1Ijoib3Jzem9sa2ExMDgiLCJhIjoiY2pqcHppMDZpMjFkajNrbzFsbGpvZmpveSJ9.I3mzOw5g7M2Dz0P6JmyXPg"
 });
 
 
 
-
 class MainPage extends React.Component {
 
-    
+    constructor(props) {
+        super(props)
+        this.state = {
+            lat: props.lat,
+            lng: props.lng,
+            // zakupy to bedzie tablica obiektow, kazdy zakup bedzie mial informacje
+            // jak koordynaty, zamowione produkty, etc itp.
+            zakupy: []
+        }
+    }
+    // Tu przyjda zakupy i twoja lokalizacja
+    componentWillReceiveProps(noweProps) {
+        this.setState(noweProps)
+    }
     render() {
-        return (
-            <div className="page-main">
-                <div className="page-map">
-                    <Map
-                        style="mapbox://styles/mapbox/streets-v9"
-                        containerStyle={{
-                            height: "100%",
-                            width: "100%"
-                        }}>
-                        <Layer
-                            type="symbol"
-                            id="marker"
-                            layout={{ "icon-image": "marker-15" }}>
-                            <Feature coordinates={[1, 1]}/>
-                        </Layer>
-                    </Map>
-                    <button className="page-main-button button">
-                        <i className="fas fa-shopping-cart"></i> add list
-                    </button>
-                </div>
+        return <div className="page-main">
+            <div className="page-map">
+                <Map
+                    style="mapbox://styles/mapbox/streets-v9"
+                    containerStyle={{
+                        height: "696px",
+                        width: "1100px"
+                    }}
+                    zoom={[16]}
+                    center={[this.state.lng, this.state.lat]}
+                >
+                    /*Tutaj robisz petle po zakupach i wyswietlasz markery */
+                    <Layer
+                        type="symbol"
+                        id="marker"
+                        layout={{"icon-image": "marker-15", "icon-size": 3}}
+                    >
+                        <Feature coordinates={[this.state.lng, this.state.lat]}/>
+                    </Layer>
+                    /*AZ DOTĄD*/
+
+                </Map>
+                <button className="add-button page-main-button button"><i className="fas fa-shopping-cart"></i> add list</button>
             </div>
-        )
+        </div>
     }
 }
 
